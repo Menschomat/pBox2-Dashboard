705b574cf5388c2c3156b57977c55936a8fdad9b
Falco <falco@weihrauch.me>
2023-04-01 20:41:52 +0200

    fan-control working for the first time

--- src/app/services/fan.service.ts
+++ src/app/services/fan.service.ts
@@ -11,4 +11,7 @@ export class FanService {
   public getFan(boxId: string, fanId: string): Observable<Fan> {
     return this.http.get<Fan>(`/api/v1/${boxId}/fans/${fanId}`);
   }
+  public updateFan(boxId: string, fan: Fan): Observable<Fan> {
+    return this.http.post<Fan>(`/api/v1/${boxId}/fans/${fan.id}`, fan);
+  }
 }
