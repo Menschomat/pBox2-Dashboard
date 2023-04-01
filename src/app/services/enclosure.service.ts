ca76763d67753180f44bb2a13a6c7f0f68d049eb
Falco <falco@weihrauch.me>
2023-04-01 18:11:23 +0200

    Splitted Api interface into multiple services

--- src/app/services/enclosure.service.ts
+++ src/app/services/enclosure.service.ts
@@ -9,38 +9,19 @@ import { Injectable } from '@angular/core';
 export class EnclosureService {
   private enclosureSubject: BehaviorSubject<Enclosure | undefined> =
     new BehaviorSubject<Enclosure | undefined>(undefined);
-  public enclosure$: Observable<Enclosure | undefined> =
-    this.enclosureSubject.asObservable();
-  private selectedBox: BehaviorSubject<Box | undefined> = new BehaviorSubject<
-    Box | undefined
-  >(undefined);
-  public selectedBox$: Observable<Box | undefined> =
-    this.selectedBox.asObservable();
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.fetchEnclosureConfig().subscribe((enc) =>
+      this.enclosureSubject.next(enc)
+    );
+  }
 
-  getEnclosureConfig(): Observable<Enclosure> {
+  public getEnclosure$(): Observable<Enclosure | undefined> {
+    return this.enclosureSubject.asObservable();
+  }
+
+  private fetchEnclosureConfig(): Observable<Enclosure> {
     return this.http.get<Enclosure>('/api/v1/enclosure');
   }
 
-  getBoxes(): Observable<Box[]> {
-    return this.http.get<Enclosure>('/api/v1/enclosure').pipe(
-      map((enc) => enc.boxes),
-      tap((boxes) => {
-        if (this.selectedBox.value === undefined && boxes.length > 0) {
-          const possibleLastBox = boxes.find(
-            (box) => box.id == localStorage.getItem('pbox2_selected_box')
-          );
-          this.selectedBox.next(possibleLastBox ? possibleLastBox : boxes[0]);
-        }
-      })
-    );
-  }
-  selectBox(box: Box) {
-    this.selectedBox.next(box);
-  }
-  getSensorData(boxId: string, sensorId: string): Observable<TimeSeries> {
-    return this.http.get<TimeSeries>(
-      `/api/v1/${boxId}/sensors/${sensorId}/data`
-    );
-  }
+
 }
