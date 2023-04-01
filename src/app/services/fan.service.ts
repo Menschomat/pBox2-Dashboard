d2a83e2da2eca4dec5dfb1b8f565e521705de764
Falco <falco@weihrauch.me>
2023-04-01 18:28:04 +0200

    Added first version of fetchers for fans and lights

--- src/app/services/fan.service.ts
+++ src/app/services/fan.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Fan } from '../model/enclosure';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FanService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  public getFan(boxId: string, fanId: string): Observable<Fan> {
+    return this.http.get<Fan>(`/api/v1/${boxId}/fans/${fanId}`);
+  }
 }
